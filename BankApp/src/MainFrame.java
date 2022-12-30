b7debbac0ea615b7086c9da64fcf157450a278f2
Oskar <>
2022-12-30 21:05:02 +0100

    prepare for work with database cards and clients

--- BankApp/src/MainFrame.java
+++ BankApp/src/MainFrame.java
@@ -25,8 +25,37 @@ public class MainFrame extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(700, 600);
 
-        String client_nr = client_number;
 
+        welcomeDbFunction(client_number);
+
+//        wyjdzButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                dispose();
+//                LoginFrame login = new LoginFrame();
+//                login.setVisible(true);
+//            }
+//        });
+//        wplataButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                dispose();
+//                WplataFrame wplata = new WplataFrame(client_nr);
+//                wplata.setVisible(true);
+//            }
+//        });
+//        wyplataButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                dispose();
+//                WyplataFrame wyplata = new WyplataFrame();
+//                wyplata.setVisible(true);
+//            }
+//        });
+    }
+
+    private void welcomeDbFunction(String client_nr)
+    {
         try {
             Connection connection = (Connection) DriverManager.getConnection(DB_URL,
                     "root", "rootroot");
@@ -34,8 +63,15 @@ public class MainFrame extends JFrame {
             PreparedStatement st = (PreparedStatement) connection
                     .prepareStatement("SELECT * FROM BankApp.Client WHERE client_nr=?");
 
+            PreparedStatement sumSaldo = (PreparedStatement) connection
+                    .prepareStatement("SELECT ROUND(SUM(card_balance), 2) AS balance FROM CARD WHERE client_nr=?");
+
             st.setString(1, client_nr);
+            sumSaldo.setString(1, client_nr);
+
             ResultSet rs = st.executeQuery();
+            ResultSet sumSaldoResult = sumSaldo.executeQuery();
+
             if (rs.next()) {
                 // Jeśli znajdziemy usera w bazie z podanym numerem klienta:
 
@@ -47,47 +83,33 @@ public class MainFrame extends JFrame {
                 // Error 123: Data in database are not comipled!
                 JOptionPane.showMessageDialog(MainFrame.this, "Error 123!");
             }
+
+            if (sumSaldoResult.next()) {
+                // Jeśli znajdziemy usera w bazie z podanym numerem klienta:
+
+                saldoValueLabel.setText(sumSaldoResult.getString("balance"));
+
+            } else {
+                // Error 123: Data in database are not comipled!
+                JOptionPane.showMessageDialog(MainFrame.this, "Error 123!");
+            }
         } catch (SQLException sqlException) {
             // Error 12: Database is off or Your connection is invalid!
             JOptionPane.showMessageDialog(MainFrame.this, "Error 12!");
         }
-
-
-//        String[] columnNames = {"Nr Karty",
-//                "Rodzaj karty"};
-//
-//        Object[][] data = {
-//                {"14151515", "Kredytowa"},
-//                {"23523525", "Debetowa"}
-//        };
-
-//        kartyTable = new JTable(data, columnNames);
-
-        wyjdzButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose();
-                LoginFrame login = new LoginFrame();
-                login.setVisible(true);
-            }
-        });
-        wplataButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose();
-                WplataFrame wplata = new WplataFrame(client_nr);
-                wplata.setVisible(true);
-            }
-        });
-        wyplataButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose();
-                WyplataFrame wyplata = new WyplataFrame();
-                wyplata.setVisible(true);
-            }
-        });
     }
+
+//    private void createTable(){
+//        Object[][] data = {
+//                {"Gra 1", "Developer", "RPG", 5.0},
+//        };
+//
+//        String[] naglowek = {"Numer Karty", "Termin ważności", "Rodzaj karty", "Saldo na karcie"};
+//
+//        table1.setModel(new DefaultTableModel(data, naglowek));
+//
+//        //Pamiętać aby wrzucić Tabele w JScrollPane XDXDXDX
+//    }
 
 
 }
