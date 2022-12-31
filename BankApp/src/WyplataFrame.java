3dd74ba83217f4677a05fa5a3366ad74407eb91e
Oskar <>
2022-12-31 15:59:32 +0100

    add Ofervlows History and repair button for other functions

--- BankApp/src/WyplataFrame.java
+++ BankApp/src/WyplataFrame.java
@@ -7,11 +7,13 @@ public class WyplataFrame extends JFrame {
     private JTextField textField1;
     private JButton OKButton;
 
-    public WyplataFrame() {
+    public WyplataFrame(String client_nr) {
         super("WyplataFrame");
         this.setContentPane(this.panel1); // wyswietlanie okienka na ekranie
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(700, 600);
+
+        System.out.println(client_nr);
 
         OKButton.addActionListener(new ActionListener() {
             @Override
